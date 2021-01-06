fdf7b1f70d0e32ac17abc34d870318d8b7d1d8cd
daniela itzel hernandez muñoz <daithernandezmu@ittepic.edu.mx>
2021-01-06 12:26:02 -0700

    primero

---
+++ OneDrive/Documents/Hibridas/Unidad4/eje05/eje05/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'newstudent',
+    pathMatch: 'full'
+  },
+  {
+    path: 'newstudent',
+    loadChildren: () => import('./pages/new-student/new-student.module').then( m => m.NewStudentPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ OneDrive/Documents/Hibridas/Unidad4/eje05/eje05/src/app/models/estudiante.ts
@@ -0,0 +1,8 @@
+export class Estudiante {
+    name: string;
+    controlnumber: string;
+    curp: string;
+    age: number;
+    active: boolean;
+}
+
